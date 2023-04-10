f437513b29b1e4b608378b6a02ccfd14afd9d496
Azhar <mohdazharsm@gmail.com>
2023-04-10 06:53:34 +0530

    Initial commit

---
+++ ESP32-RTSP/src/wifikeys.h
@@ -0,0 +1,4 @@
+#include <Arduino.h>
+// Change YOUR_AP_NAME and YOUR_AP_PASSWORD to your WiFi credentials
+const char *ssid = "Autobonics_4G";     // Put your SSID here
+const char *password = "autobonics@27"; // Put your PASSWORD here
